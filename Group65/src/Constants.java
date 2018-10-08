f3caa52a24ee54867f846c049596eea8ce665afe
pollendo <ollendorff.philipp@gmail.com>
2018-10-08 14:18:48 +0200

    We can now submit and achieve actual scores. Test function is around 9, all others between 0 and 2.

--- Group65/src/Constants.java
+++ Group65/src/Constants.java
@@ -8,19 +8,19 @@ public class Constants{
 
     public final static int FITTEST_SIZE = POPULATION_SIZE - RECOMBINATION_SIZE - MUTATION_SIZE;
 
-    public final static int CYCLES_SIZE = 100;
+    public final static int CYCLES_SIZE = 1000;
 
-    public final static int LAST_CYCLES_WITHOUT_MUTATION = 10;
+    public final static int LAST_CYCLES_WITHOUT_MUTATION = 100;
 
-    public final static MutationType CURRENT_MUTATION_TYPE = MutationType.BINARY;
+    public final static MutationType CURRENT_MUTATION_TYPE = MutationType.DOUBLE;
 
     public final static ParentSelectionType CURRENT_PARENT_SELECTION_TYPE = ParentSelectionType.ROULETTE_WHEEL;
     
     public final static PhenotypeRepresentation CURRENT_PHENOTYPE_REPRESENTATION = PhenotypeRepresentation.DOUBLE;
     
-    public final static GenotypeRepresentation CURRENT_GENOTYPE_REPRESENTATION = GenotypeRepresentation.BINARY;
+    public final static GenotypeRepresentation CURRENT_GENOTYPE_REPRESENTATION = GenotypeRepresentation.DOUBLE;
 
     public final static int BINARY_REPRESENTATION_LENGTH = 5;
 
-    public final static int MAX_PHENOTYPE_NUMBER_SIZE = 31;
+    public final static int MAX_PHENOTYPE_NUMBER_SIZE = 1;
 }
