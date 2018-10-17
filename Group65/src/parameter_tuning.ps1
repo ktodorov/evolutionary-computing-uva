605c175e9111547a22d8d0e5b8c7a667fde83885
Konstantin Todorov <kztodorov@outlook.com>
2018-10-17 02:34:38 +0200

    Improved script with statistics from the evaluation

--- Group65/src/parameter_tuning.ps1
+++ Group65/src/parameter_tuning.ps1
@@ -1,21 +1,79 @@
+function CalculateAverage
+{
+    Param ([double[]]$array)
+
+    $sum = 0
+    foreach ($item in $array)
+    {
+        $sum += $item
+    }
+
+    $result = ($sum / $array.Length)
+    return $result
+}
+
+function CalculateStandardDeviation
+{
+    Param ([double[]]$array)
+
+    $average = CalculateAverage $array
+    $numerator = 0.0
+    foreach ($item in $array)
+    {
+        $numerator += [System.Math]::Pow($item - $average, 2)
+    }
+
+    $result = [System.Math]::Sqrt($numerator / ($array.Length - 1))
+    return $result
+}
+
 function InvokeFunction
 {
 
-    Param ([int]$populationSize, [int]$fittestSize, [int]$recombinationSize, [int]$mutationSize, [string]$function)
+    Param ([int]$populationSize, [int]$fittestSize, [int]$recombinationSize, [int]$mutationSize, [string]$function, [string]$parentSelectionType, [int]$runs)
+
+    Write-Host "Starting $function with selection: $parentSelectionType"
 
     $maxScore = 0
     $minScore = -1
     $avgScore = -1
+    $avgCycleNumber = -1
     $i = 0
-    while ($i -lt 100)
+    $cyclesCount = 100
+    $scores = @()
+    $cycles = @()
+    $maxFitnessPerCycle = @()
+
+    for ($j = 0; $j -lt $cyclesCount; $j++)
     {
-        $command = '& java -DpopulationSize=' + $populationSize + ' -DfittestSize=' + $fittestSize + ' -DrecombinationSize=' + $recombinationSize + ' -DmutationSize=' + $mutationSize + " -jar testrun.jar -submission=player65 -evaluation=" + $function + ' -seed=1 2>&1'
+        $maxFitnessPerCycle += 0.0
+    }
+
+    while ($i -lt $runs)
+    {      
+        $command = '& java -DpopulationSize=' + $populationSize + ' -DfittestSize=' + $fittestSize + ' -DrecombinationSize=' + 
+                    $recombinationSize + ' -DmutationSize=' + $mutationSize + " -DparentSelectionType=" + $parentSelectionType +
+                    " -jar testrun.jar -submission=player65 -evaluation=" + $function + ' -seed=1 2>&1'
         # Write-Host $command
         $output = Invoke-Expression $command
         # Write-Host $output
         $splittedOutput = $output.split(' ')
+        
+        $maxFitnessPerCycle[0] = ($maxFitnessPerCycle[0], [System.Decimal]$splittedOutput[0] | Measure-Object -Max).Maximum
+
+        for ($j = 1; $j -lt $cyclesCount; $j++)
+        {
+            $newDecimal = [System.Decimal]$splittedOutput[$j]
+            $maxFitnessPerCycle[$j] = ($maxFitnessPerCycle[$j], $newDecimal, $maxFitnessPerCycle[$j - 1] | Measure-Object -Max).Maximum
+        }
+
         # Write-Host "splitted output length - " $splittedOutput.length
         $currentScore = [System.Decimal]($splittedOutput[$splittedOutput.Length - 3])
+        $scores += $currentScore
+
+        $cycleNumber = [System.Decimal]($splittedOutput[$splittedOutput.Length - 5])
+        $cycles += $cycleNumber
+
         # Write-Host "Current score - " $currentScore
         if ($maxScore -lt $currentScore)
         {
@@ -36,16 +94,27 @@ function InvokeFunction
             $avgScore = ($currentScore + $avgScore)/2
         }
 
+        if ($avgCycleNumber -eq -1) 
+        {
+            $avgCycleNumber = $cycleNumber
+        }
+        else 
+        {
+            $avgCycleNumber = ($cycleNumber + $avgCycleNumber)/2
+        }
+
         $i++
-        Start-Sleep -Seconds 1
     }
 
-    return $maxScore, $minScore, $avgScore
+    $scoresStandardDeviation = CalculateStandardDeviation $scores
+    $cyclesStandardDeviation = CalculateStandardDeviation $cycles
+
+    return $maxScore, $minScore, $avgScore, $avgCycleNumber, $scoresStandardDeviation, $cyclesStandardDeviation, $maxFitnessPerCycle
 }
 
 function TuneFunction
 {
-    Param ([string]$function)
+    Param ([string]$function, [string]$parentSelectionType)
     $maxScore = 0
     $populationSize = 45
     $bestPopulationSize = 0
@@ -60,7 +129,7 @@ function TuneFunction
         {
             For ($k = 0; $k -le $populationSize - $i - $j -and $k -le 15; $k+=2)
             {
-                $currentMaxScore, $currentMinScore, $currentScore = InvokeFunction -populationSize $populationSize -fittestSize $i -recombinationSize $j -mutationSize $k -function $function
+                $currentMaxScore, $currentMinScore, $currentScore = InvokeFunction -populationSize $populationSize -fittestSize $i -recombinationSize $j -mutationSize $k -function $function -parentSelectionType $parentSelectionType
                 # Write-Host "current score - " $currentScore
                 if ($maxScore -lt $currentScore)
                 {
@@ -83,6 +152,106 @@ function TuneFunction
     return $maxScore, $bestPopulationSize, $bestFittestSize, $bestRecombinationSize, $bestMutationSize
 }
 
+function WriteFunctionResult
+{
+    Param (
+    [string]$parentSelectionType, 
+    [int]$populationSize, 
+    [int]$fittestSize, 
+    [int]$recombinationSize, 
+    [int]$mutationSize, 
+    [string]$function, 
+    [double]$minScore, 
+    [double]$maxScore, 
+    [double]$avgScore, 
+    [double]$avgCycleNumber, 
+    [double]$scoresStandardDeviation, 
+    [double]$cyclesStandardDeviation, 
+    [double[]]$maxFitnessPerCycle)
+
+    Write-Host 
+"Parent selection type: $parentSelectionType`npopulation size: $populationSize`nfittest size: $fittestSize
+recombination size: $recombinationSize`nmutation size: $mutationSize`nfunction: $function`n - min: $minScore
+ - max: $maxScore`n - avg: $avgScore`n - avg cycle found: $avgCycleNumber`n - scores standard deviation: $scoresStandardDeviation
+ - cycles standard deviation: $cyclesStandardDeviation`n - max fitnesses per cycle: $maxFitnessPerCycle"
+    
+}
+
+function SaveToExcel
+{
+    Param (
+    [string]$parentSelectionType, 
+    [int]$populationSize, 
+    [int]$fittestSize, 
+    [int]$recombinationSize, 
+    [int]$mutationSize, 
+    [string]$function, 
+    [double]$minScore, 
+    [double]$maxScore, 
+    [double]$avgScore, 
+    [double]$avgCycleNumber, 
+    [double]$scoresStandardDeviation, 
+    [double]$cyclesStandardDeviation, 
+    [double[]]$maxFitnessPerCycle)
+
+    $excel = New-Object -ComObject Excel.Application 
+    $excel.Visible = $True
+    $workbook = $excel.Workbooks.Add()
+    $firstWorksheet = $workbook.Worksheets.Item(1) 
+
+    $firstWorksheet.Cells.Item(1,1) = "Parent selection type"
+    $firstWorksheet.Cells.Item(1,2) = "Population size"
+    $firstWorksheet.Cells.Item(1,3) = "Fittest size"
+    $firstWorksheet.Cells.Item(1,4) = "Recombination size"
+    $firstWorksheet.Cells.Item(1,5) = "Mutation size"
+    $firstWorksheet.Cells.Item(1,6) = "Function"
+    $firstWorksheet.Cells.Item(1,7) = "Minimum score"
+    $firstWorksheet.Cells.Item(1,8) = "Maximum score"
+    $firstWorksheet.Cells.Item(1,9) = "Average score"
+    $firstWorksheet.Cells.Item(1,10) = "Average cycle number"
+    $firstWorksheet.Cells.Item(1,11) = "Standard deviation of scores"
+    $firstWorksheet.Cells.Item(1,12) = "Standard deviation of cycles"
+
+    $firstWorksheet.Cells.Item(2, 1) = $parentSelectionType
+    $firstWorksheet.Cells.Item(2, 2) = $populationSize
+    $firstWorksheet.Cells.Item(2, 3) = $fittestSize
+    $firstWorksheet.Cells.Item(2, 4) = $recombinationSize
+    $firstWorksheet.Cells.Item(2, 5) = $mutationSize
+    $firstWorksheet.Cells.Item(2, 6) = $function
+    $firstWorksheet.Cells.Item(2, 7) = $minScore
+    $firstWorksheet.Cells.Item(2, 8) = $maxScore
+    $firstWorksheet.Cells.Item(2, 9) = $avgScore
+    $firstWorksheet.Cells.Item(2, 10) = $avgCycleNumber
+    $firstWorksheet.Cells.Item(2, 11) = $scoresStandardDeviation
+    $firstWorksheet.Cells.Item(2, 12) = $cyclesStandardDeviation
+
+    $firstWorksheet.Cells.Item(4,1) = "Max fitnesses per cycle"
+
+    $col = 1
+    foreach ($item in $maxFitnessPerCycle)
+    {
+        $firstWorksheet.Cells.Item(5,$col) = $item
+        $col++
+    }
+
+    $firstWorksheet.activate()
+    $maxFitnessesDataSheet = $firstWorksheet.Range("A5").CurrentRegion
+
+    $chart = $firstWorksheet.Shapes.AddChart().Chart
+    $chart.ChartType = [Microsoft.Office.Interop.Excel.XLChartType]::xlLine
+    $chart.SetSourceData($maxFitnessesDataSheet)
+
+    $usedRange = $firstWorksheet.UsedRange
+    $usedRange.EntireColumn.AutoFit() | Out-Null
+
+    $xlFixedFormat = [Microsoft.Office.Interop.Excel.XlFileFormat]::xlWorkbookDefault
+    $outputPath = "C:\EC_Stats\$function" + "_$parentSelectionType.xlsx"
+    $workbook.SaveAs($outputPath, $xlFixedFormat) 
+    $workbook.Close()
+    $excel.DisplayAlerts = $False
+    $excel.Quit()
+}
+
 Write-Host "Starting parameter tuning:"
 
 Write-Host "Compiling..."
@@ -94,18 +263,93 @@ if (!([string]::IsNullOrEmpty($compilationOutput)))
     return
 }
 
+function ExecuteFunctionAndSaveReport
+{
+    Param([int]$populationSize, [int]$fittestSize, [int]$recombinationSize, [int]$mutationSize, [string]$function, [string]$parentSelectionType, [int]$runs)
+
+    $maxScore, $minScore, $avgScore, $avgCycleNumber, $scoresStandardDeviation, $cyclesStandardDeviation, $maxFitnessPerCycle = & InvokeFunction $populationSize $fittestSize $recombinationSize $mutationSize $function $parentSelectionType $runs 2>&1
+    SaveToExcel $parentSelectionType $populationSize $fittestSize $recombinationSize $mutationSize $function $minScore $maxScore $avgScore $avgCycleNumber $scoresStandardDeviation $cyclesStandardDeviation $maxFitnessPerCycle
+}
+
 jar cmf MainClass.txt submission.jar player65.class *.class
-jar uf submission.jar player65.class *.class
+jar uf testrun.jar player65.class *.class
 
 # TuneFunction -function SphereEvaluation
 # TuneFunction -function BentCigarFunction
 # TuneFunction -function SchaffersEvaluation
 # TuneFunction -function KatsuuraEvaluation
-# $maxScore, $minScore, $avgScore = & InvokeFunction -populationSize 32 -fittestSize 4 -recombinationSize 16 -mutationSize 12 -function SphereEvaluation 2>&1
-# Write-Host "Sphere: min - " $minScore "| max - " $maxScore "| avg - $avgScore"
-# $maxScore, $minScore, $avgScore = & InvokeFunction -populationSize 34 -fittestSize 6 -recombinationSize 16 -mutationSize 12 -function BentCigarFunction 2>&1
-# Write-Host "Bent cigar: min - $minScore | max - $maxScore | avg - $avgScore"
-# $maxScore, $minScore, $avgScore = & InvokeFunction -populationSize 45 -fittestSize 0 -recombinationSize 38 -mutationSize 6 -function SchaffersEvaluation 2>&1
-# Write-Host "Schaffers: min - $minScore | max - $maxScore | avg - $avgScore"
-# $maxScore, $minScore = & InvokeFunction -populationSize 45 -fittestSize 3 -recombinationSize 36 -mutationSize 6 -function KatsuuraEvaluation 2>&1
-# Write-Host "Katsuura: min - " $minScore "| max - " $maxScore
+$parentSelectionType = "TOURNAMENT"
+$populationSize = 100
+$fittestSize = 20
+$recombinationSize = 40
+$mutationSize = 40
+$runs = 100
+
+# $function = "BentCigarFunction"
+# ExecuteFunctionAndSaveReport $populationSize $fittestSize $recombinationSize $mutationSize $function $parentSelectionType $runs
+
+
+# $populationSize = 32
+# $fittestSize = 4
+# $recombinationSize = 16
+# $mutationSize = 12
+$function = "SphereEvaluation"
+ExecuteFunctionAndSaveReport $populationSize $fittestSize $recombinationSize $mutationSize $function $parentSelectionType $runs
+
+# # $populationSize = 34
+# # $fittestSize = 6
+# # $recombinationSize = 16
+# # $mutationSize = 12
+$function = "BentCigarFunction"
+ExecuteFunctionAndSaveReport $populationSize $fittestSize $recombinationSize $mutationSize $function $parentSelectionType $runs
+
+# # $populationSize = 45
+# # $fittestSize = 0
+# # $recombinationSize = 38
+# # $mutationSize = 6
+$function = "SchaffersEvaluation"
+ExecuteFunctionAndSaveReport $populationSize $fittestSize $recombinationSize $mutationSize $function $parentSelectionType $runs
+
+# $populationSize = 45
+# $fittestSize = 3
+# $recombinationSize = 36
+# $mutationSize = 6
+$function = "KatsuuraEvaluation"
+$runs = 10
+ExecuteFunctionAndSaveReport $populationSize $fittestSize $recombinationSize $mutationSize $function $parentSelectionType $runs
+$runs = 100
+
+
+# # Random selection
+$parentSelectionType = "RANDOM"
+$function = "SphereEvaluation"
+ExecuteFunctionAndSaveReport $populationSize $fittestSize $recombinationSize $mutationSize $function $parentSelectionType $runs
+
+$function = "BentCigarFunction"
+ExecuteFunctionAndSaveReport $populationSize $fittestSize $recombinationSize $mutationSize $function $parentSelectionType $runs
+
+$function = "SchaffersEvaluation"
+ExecuteFunctionAndSaveReport $populationSize $fittestSize $recombinationSize $mutationSize $function $parentSelectionType $runs
+
+$runs = 10
+$function = "KatsuuraEvaluation"
+ExecuteFunctionAndSaveReport $populationSize $fittestSize $recombinationSize $mutationSize $function $parentSelectionType $runs
+$runs = 100
+
+
+
+# # Roulette wheel selection
+$parentSelectionType = "ROULETTE_WHEEL"
+$function = "SphereEvaluation"
+ExecuteFunctionAndSaveReport $populationSize $fittestSize $recombinationSize $mutationSize $function $parentSelectionType $runs
+
+$function = "BentCigarFunction"
+ExecuteFunctionAndSaveReport $populationSize $fittestSize $recombinationSize $mutationSize $function $parentSelectionType $runs
+
+$function = "SchaffersEvaluation"
+ExecuteFunctionAndSaveReport $populationSize $fittestSize $recombinationSize $mutationSize $function $parentSelectionType $runs
+
+$runs = 10
+$function = "KatsuuraEvaluation"
+ExecuteFunctionAndSaveReport $populationSize $fittestSize $recombinationSize $mutationSize $function $parentSelectionType $runs
+$runs = 100
